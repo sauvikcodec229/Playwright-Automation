52e647001503e5a23d66c3ebfd354e5f3491288a
Sarkar <sauvsarkar@deloitte.com>
2026-01-05 22:02:13 +0530

    Fixed wrong path for apiUtils

--- tests/WebAPI_2.spec.js
+++ tests/WebAPI_2.spec.js
@@ -1,5 +1,5 @@
 const {test, expect,request} = require('@playwright/test');
-const {APiUtils} = require('./utils/APiUtils');
+const {APiUtils} = require('../utils/APiUtils');
 
 
 //we are creating this JSON payload as one javascript object
